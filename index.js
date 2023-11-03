f0adb8ba9fca591199d89fd1972e47d1fd9cfd5f
Nikhil Varshney <nikhilnikvarshney007@gmail.com>
2023-11-04 01:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,13 +24,13 @@ const connectDB = async () => {
 dotenv.config()
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "/images")))
-// app.use(
-//   cors({
-//     origin: "https://techiees.netlify.app",
-//     credentials: true,
-//   })
-// )
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(
+  cors({
+    origin: "https://hiringhelpers.netlify.app/",
+     credentials: true,
+  })
+ )
+//app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 //Routes
 app.use("/api/auth", authRoute)
